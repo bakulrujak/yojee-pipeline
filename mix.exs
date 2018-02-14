eed9d1adeaab3bf6096e9b0ccb89b22dad538eda
Andika Kurniantoro <deekz.it@gmail.com>
2018-02-14 23:46:09 +0700

    Add codebuild integration

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule YojeeChatExample.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
 			{:cowboy, "~> 1.0"},
-			{:libcluster, "~> 2.1"}
+      {:distillery, "~> 1.5", runtime: false},
+			{:libcluster_ec2, "~> 0.2"}
     ]
   end
 
