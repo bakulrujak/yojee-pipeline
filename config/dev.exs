e3ddcb857ce5723812bc23a79e3d4a7747ee52cf
Andika Kurniantoro <deekz.it@gmail.com>
2018-02-15 10:04:33 +0700

    Add NPM support

--- config/dev.exs
+++ config/dev.exs
@@ -52,6 +52,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :yojee_chat_example, YojeeChatExample.Repo,
   adapter: Ecto.Adapters.Postgres,
-  database: "yojee_chat_example_dev",
+  username: "postgres"
+  password: "d0ntt3ll"
+  database: "postgres",
   hostname: "localhost",
   pool_size: 10
