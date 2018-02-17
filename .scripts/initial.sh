38770a652917e8eb1a4768773472ea3755bec277
Andika Kurniantoro <deekz.it@gmail.com>
2018-02-17 22:10:20 +0700

    Fix file location

--- .scripts/initial.sh
+++ .scripts/initial.sh
@@ -2,9 +2,9 @@
 
 export LC_ALL="en_US.UTF-8"
 
-sh .scripts/env.sh
+sh env.sh
 mix local.rebar --force
 mix local.hex --force
 mix deps.get
 mix ecto.migrate > /tmp/migrate.txt
-cd assets && npm install >> /tmp/migrate.txt
+cd ../assets && npm install >> /tmp/migrate.txt
--- .scripts/startme.sh
+++ .scripts/startme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export LC_ALL="en_US.UTF-8"
-sh .scripts/env.sh && mix phx.server
+sh env.sh && mix phx.server
