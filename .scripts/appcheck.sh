aa627d32fef75fa044aed0b090d8d1dd9c24b0b9
Andika Kurniantoro <deekz.it@gmail.com>
2018-02-17 23:20:53 +0700

    Add interval

--- .scripts/appcheck.sh
+++ .scripts/appcheck.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+sleep 5
 if [ $(netstat -tlpn | grep 8080 | wc -l) != 1 ]; then exit 1; else exit 0; fi
--- .scripts/startme.sh
+++ .scripts/startme.sh
@@ -2,5 +2,5 @@
 
 export LC_ALL="en_US.UTF-8"
 
-cd /home/ubuntu/app
-sh .scripts/env.sh && mix phx.server &
+cd /home/ubuntu/app && sh .scripts/env.sh
+mix phx.server &
