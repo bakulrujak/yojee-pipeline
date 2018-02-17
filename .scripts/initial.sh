452b25382a2caea106f72267de943b0514937382
Andika Kurniantoro <deekz.it@gmail.com>
2018-02-18 00:26:11 +0700

    change it to Install phase

--- .scripts/initial.sh
+++ .scripts/initial.sh
@@ -2,9 +2,10 @@
 
 export LC_ALL="en_US.UTF-8"
 
+cd /home/ubuntu/app
 sh .scripts/env.sh
 mix local.rebar --force
 mix local.hex --force
 mix deps.get
 mix ecto.migrate > /tmp/migrate.txt
-cd ../assets && npm install >> /tmp/migrate.txt
+cd assets && npm install >> /tmp/migrate.txt
