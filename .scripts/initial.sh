dcc9fa12b561691451175c1820503e9692f7a826
Andika Kurniantoro <deekz.it@gmail.com>
2018-02-17 22:17:35 +0700

    Fix another file locs

--- .scripts/initial.sh
+++ .scripts/initial.sh
@@ -3,8 +3,9 @@
 export LC_ALL="en_US.UTF-8"
 
 sh env.sh
+cd ../
 mix local.rebar --force
 mix local.hex --force
 mix deps.get
 mix ecto.migrate > /tmp/migrate.txt
-cd ../assets && npm install >> /tmp/migrate.txt
+cd assets && npm install >> /tmp/migrate.txt
