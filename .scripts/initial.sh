d1e8125b60bf28a580389194e371ece50da9f14c
Andika Kurniantoro <deekz.it@gmail.com>
2018-02-17 13:39:21 +0700

    Enhance security

--- .scripts/initial.sh
+++ .scripts/initial.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export LC_ALL="en_US.UTF-8"
+
 cd assets && npm install
 Mix=dev mix ecto.migrate
 
--- config/dev.exs
+++ config/dev.exs
@@ -67,7 +67,7 @@ config :yojee_chat_example, YojeeChatExample.Repo,
   pool_size: 10
 
 config :ex_aws,
-  access_key_id: "ABC",
-  secret_access_key: "XYZ",
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
   debug_requests: true,
   region: "us-east-1"  
